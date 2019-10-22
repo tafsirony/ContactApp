17da5d0e5c4596f002b301c26b01c4bd54f59e8b
Tafsir Ony <onykhan95@gmail.com>
2019-10-23 01:47:44 +0600

    Activity 1,2,3

--- app/src/main/java/com/example/contactapp/Main2Activity.java
+++ app/src/main/java/com/example/contactapp/Main2Activity.java
@@ -2,15 +2,55 @@ package com.example.contactapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity  {
 
+    ImageView imageView1,imageView2,imageView3;
+    TextView contactView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        contactView = findViewById(R.id.contactView);
+
+        imageView1 = findViewById(R.id.smile);
+        imageView2 = findViewById(R.id.sad);
+        imageView3 = findViewById(R.id.astonishedEye);
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+
+
     }
+    public void openActivity3(){
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -6,14 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button createContact;
+    TextView contactView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        contactView = findViewById(R.id.contactView);
         createContact = findViewById(R.id.createContact);
         createContact.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,9 +23,17 @@ public class MainActivity extends AppCompatActivity {
                 openActivity2();
             }
         });
+        //contactView.setVisibility(View.GONE);
+        String name = getIntent().getStringExtra("Name");
+        String phone = getIntent().getStringExtra("Phone");
+        String address = getIntent().getStringExtra("address");
+        String website = getIntent().getStringExtra("website");
+        contactView = findViewById(R.id.contactView);
+        contactView.setText("Name: "+name+"\nphone: "+phone+"\naddress: "+address+"\nwebsite: "+website);
     }
     public void openActivity2(){
         Intent intent = new Intent(this,Main2Activity.class);
         startActivity(intent);
+
     }
 }
