a099d7fce0fe96bd47cb6717d500f1093e5b5e86
Tafsir Ony <tafsirony@users.noreply.github.com>
2019-10-23 09:47:40 +0600

    Update Main2Activity.java

--- app/src/main/java/com/example/contactapp/Main2Activity.java
+++ app/src/main/java/com/example/contactapp/Main2Activity.java
@@ -8,7 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.EditText;
+import com.example.contactapp.R;
 public class Main2Activity extends AppCompatActivity  {
 
     ImageView imageView1,imageView2,imageView3;
