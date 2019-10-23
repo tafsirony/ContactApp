dd25348be303e14d3162b65702d9c3770fcecd02
Tafsir Ony <tafsirony@users.noreply.github.com>
2019-10-23 09:39:34 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/contactapp/MainActivity.java
+++ app/src/main/java/com/example/contactapp/MainActivity.java
@@ -17,19 +17,47 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         contactView = findViewById(R.id.contactView);
         createContact = findViewById(R.id.createContact);
+        
+        contactView = findViewById(R.id.contactView);
+        contactView.setVisibility(View.GONE);
+        
         createContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity2();
             }
         });
-        //contactView.setVisibility(View.GONE);
+        // You can be pretty confident that the intent will not be null here.
+        Intent intent = getIntent();
+
+        // Get the extras (if there are any)
+        Bundle extras = intent.getExtras();
+        String data="";
+        if (extras != null) {
+            if (extras.containsKey("name")) {
+                //contactView.setVisibility(View.GONE);
+                String name = extras.getString("name");
+                String phone = extras.getString("phone");
+                String address = extras.getString("address");
+                String website = extras.getString("website");
+                //contactView.setText("New Data");
+                contactView.setText("Name: "+name+"\nphone: "+phone+"\naddress: "+address+"\nwebsite: "+website);
+                contactView.setVisibility(View.VISIBLE);
+                // TODO: Do something with the value of isNew.
+            }
+        }
+        
+        /*contactView.setText(data);
+        
+         
+        contactView.setVisibility(View.GONE);
         String name = getIntent().getStringExtra("Name");
         String phone = getIntent().getStringExtra("Phone");
         String address = getIntent().getStringExtra("address");
         String website = getIntent().getStringExtra("website");
         contactView = findViewById(R.id.contactView);
         contactView.setText("Name: "+name+"\nphone: "+phone+"\naddress: "+address+"\nwebsite: "+website);
+        */
     }
     public void openActivity2(){
         Intent intent = new Intent(this,Main2Activity.class);
